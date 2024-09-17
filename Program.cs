fde06692efb53331b795ee89dbeb0849f2ee60b1
IHtDzenda <nev27im@gmail.com>
2024-09-17 12:30:13 +0200

    feat: debug logging image merging

--- Program.cs
+++ Program.cs
@@ -1,24 +1,23 @@
 ﻿using Core.Api.Maps;
-using Spectre.Console;
+using Core.Debug;
 public class Program
 {
   static void Main(string[] args)
   {
-    double latitude = 50.0755;
-    double longitude = 14.4378;
+
+    double latitude = 50.0715200;
+    double longitude = 14.403497;
     Console.WriteLine($"Location: Prague, Czech Republic | Latitude = {latitude}, Longitude = {longitude}");
-    string path = GetOpenStreetMapTileUrl(latitude, longitude, 16); // Zoom level 10
-    var image = new CanvasImage(path);
-    image.MaxWidth(20);
-    AnsiConsole.Write(image);
+    Tile[] tiles = GetOpenStreetMapTileUrl(latitude, longitude, 16); // Zoom level 10
+
+
   }
 
-  static string GetOpenStreetMapTileUrl(double latitude, double longitude, int zoom)
+  static Tile[] GetOpenStreetMapTileUrl(double latitude, double longitude, int zoom)
   {
-    int tileX = (int)((longitude + 180.0) / 360.0 * (1 << zoom));
-    int tileY = (int)((1.0 - Math.Log(Math.Tan(latitude * Math.PI / 180.0) + 1.0 / Math.Cos(latitude * Math.PI / 180.0)) / Math.PI) / 2.0 * (1 << zoom));
     MapsApi mapsApi = new MapsApi(MapProviders.MapPropiversName.Thunderforest);
-    return mapsApi.GetTiles(latitude, longitude, zoom);
+    Tile[] tiles = mapsApi.GetNeighbourTiles(latitude, longitude, zoom);
+    mapsApi.ConcatImages(tiles);
+    return tiles;
   }
-
 }
