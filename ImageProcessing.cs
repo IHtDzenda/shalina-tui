428c85c1831d916fc8f92ea8f5a7161f6f58d88e
JerrySB03 <gest.2513@gmail.com>
2024-09-18 17:40:40 +0200

    Feat: rivers, plains, buses

--- ImageProcessing.cs
+++ ImageProcessing.cs
@@ -5,35 +5,70 @@ namespace Core
 {
   public class ImageProcessing
   {
-    static bool PassThreshold(Rgba32 col)
+    static Rgba32? PassThreshold(Rgba32 col)
     {
-      if ((col.R > 215 && col.G < 190 && col.B < 190) || col.R < 120 && col.G < 120 && col.B < 120)
+      if ((col.R > 190 && col.G < 120 && col.B < 120 && col.G > 40 && col.B > 40) || (col.R == 246))
       {
-        return true;
+        return SixLabors.ImageSharp.Color.Red;
       }
-      return false;
+
+      else if (col.R < 120 && col.R == col.G && col.G == col.B)
+      {
+        return SixLabors.ImageSharp.Color.Black;
+      }
+
+      else if (col.R < 190 && col.G < 230 & col.B == 255)
+      {
+        return SixLabors.ImageSharp.Color.SkyBlue;
+      }
+
+      else if (col.R == 210 && col.G == 242 & col.B == 215)
+      {
+        return SixLabors.ImageSharp.Color.PaleGreen;
+      }
+
+      return null;
     }
     public static Image Threshold(Image<Rgba32> img, Int16 resolution)
     {
-      var outImg = new Image<Rgba32>(resolution, resolution, new Rgba32(255, 255, 255, 255));
+      resolution = (short) (resolution * 1);
+      var outImg = new Image<Rgba32>(resolution, resolution, new Rgba32(247, 247, 247, 255));
       for (int x = 0; x < outImg.Width; x++)
       {
         for (int y = 0; y < outImg.Height; y++)
         {
-          Int16 count = 0;
+
+          Rgba32?[] colors = new Rgba32?[(img.Width / outImg.Width) * (img.Height / outImg.Height)];
+
           for (int testX = 0; testX < (img.Width / outImg.Width); testX++)
           {
             for (int testY = 0; testY < (img.Height / outImg.Height); testY++)
             {
               //outImg[x,y] = img[(int)(x * ((double)img.Width/(double)outImg.Width)), (int)(y* ((double)img.Height/(double)outImg.Height))];
-              if (PassThreshold(img[(int)(x * ((double)img.Width / (double)outImg.Width)) + testX, (int)(y * ((double)img.Height / (double)outImg.Height)) + testY]))
-              {
-                count++;
-              }
+              colors[testX * (img.Height / outImg.Height) + testY] = (PassThreshold(img[(int)(x * ((double)img.Width / (double)outImg.Width)) + testX, (int)(y * ((double)img.Height / (double)outImg.Height)) + testY]));
             }
           }
-          if( count > img.Height / outImg.Height)
-                outImg[x, y] = SixLabors.ImageSharp.Color.Black;
+          try
+          {
+            var possiblePixelColors = colors.Where(col => col != null)
+              .GroupBy(n => n)
+              .Where(g => Math.Pow(g.Count(), 2) > (img.Width / outImg.Width) * (img.Height / outImg.Height) )
+              .OrderByDescending(g => g.Count())
+              .OrderByDescending(g => (g.First() == SixLabors.ImageSharp.Color.SkyBlue || g.First() == SixLabors.ImageSharp.Color.PaleGreen) ? 0 : 1);
+            if(possiblePixelColors.Any(g => g.Key == SixLabors.ImageSharp.Color.Red) && possiblePixelColors.Any(g => g.Key == SixLabors.ImageSharp.Color.Black) ){
+              outImg[x, y] = SixLabors.ImageSharp.Color.DarkRed;
+              continue;
+            }
+            Rgba32? newPixel = possiblePixelColors.First()?.Key;
+            if (newPixel != null)
+            {
+              outImg[x, y] = newPixel.Value;
+            }
+          }
+          catch
+          {
+            continue;
+          }
         }
       }
       return outImg;
