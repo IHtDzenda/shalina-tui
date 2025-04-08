1a3bd19e7556c423c5308fecfd91af35e8a70f7c
Kaktus1549 <hummelotup@seznam.cz>
2025-04-08 22:21:05 +0200

    fix: why is float serialized with ,

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ public class Program
 {
   static async Task Main(string[] args)
   {
+    var culture = (System.Globalization.CultureInfo) System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+    culture.NumberFormat.NumberDecimalSeparator = ".";
+    System.Threading.Thread.CurrentThread.CurrentCulture = culture;
     await Frontend.RenderUI();
   }
 }
