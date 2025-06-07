df1a66479390c64b2121d6b835f9e31fea6f6bb6
JerrySB03 <gest.2513@gmail.com>
2025-06-07 23:28:45 +0200

    feat: add subpixel

--- CanvasImageWithText/CanvasImageWithText.cs
+++ CanvasImageWithText/CanvasImageWithText.cs
@@ -3,6 +3,8 @@ using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Processing.Processors.Transforms;
 using Spectre.Console.Rendering;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
 
 
 namespace Core.Rendering
@@ -34,18 +36,21 @@ namespace Core.Rendering
     public int? MaxWidth { get; set; }
     public int PixelWidth { get; set; } = 2;
     public IResampler? Resampler { get; set; }
-    internal SixLabors.ImageSharp.Image<Rgb24> Image { get; }
+    internal SixLabors.ImageSharp.Image<Rgb24> Image { get; private set; }
+    internal SixLabors.ImageSharp.Image<Rgba32> SubPixels { get; private set; }
 
     internal Stack<CanvasText>[,] texts;
 
     public CanvasImageWithText(string filename)
     {
       Image = SixLabors.ImageSharp.Image.Load<Rgb24>(filename);
+      SubPixels = new SixLabors.ImageSharp.Image<Rgba32>(this.Width, this.Height, new Rgba32(0, 0, 0, 0));
       this.texts = new Stack<CanvasText>[this.Width, this.Height];
     }
     public CanvasImageWithText(string filename, CanvasText[] texts)
     {
       Image = SixLabors.ImageSharp.Image.Load<Rgb24>(filename);
+      SubPixels = new SixLabors.ImageSharp.Image<Rgba32>(this.Width, this.Height, new Rgba32(0, 0, 0, 0));
       this.texts = new Stack<CanvasText>[this.Width, this.Height];
       foreach (CanvasText text in texts)
       {
@@ -56,22 +61,26 @@ namespace Core.Rendering
     public CanvasImageWithText(ReadOnlySpan<byte> data)
     {
       Image = SixLabors.ImageSharp.Image.Load<Rgb24>(data);
+      SubPixels = new SixLabors.ImageSharp.Image<Rgba32>(this.Width, this.Height, new Rgba32(0, 0, 0, 0));
       this.texts = new Stack<CanvasText>[this.Width, this.Height];
     }
 
     public CanvasImageWithText(Stream data)
     {
       Image = SixLabors.ImageSharp.Image.Load<Rgb24>(data);
+      SubPixels = new SixLabors.ImageSharp.Image<Rgba32>(this.Width, this.Height, new Rgba32(0, 0, 0, 0));
       this.texts = new Stack<CanvasText>[this.Width, this.Height];
     }
     public CanvasImageWithText(SixLabors.ImageSharp.Image<Rgb24> image)
     {
       Image = image;
-      this.texts = new  Stack<CanvasText>[this.Width, this.Height];
+      SubPixels = new SixLabors.ImageSharp.Image<Rgba32>(this.Width, this.Height, new Rgba32(0, 0, 0, 0));
+      this.texts = new Stack<CanvasText>[this.Width, this.Height];
     }
     public CanvasImageWithText(SixLabors.ImageSharp.Image<Rgb24> image, CanvasText[] texts)
     {
       Image = image;
+      SubPixels = new SixLabors.ImageSharp.Image<Rgba32>(this.Width, this.Height, new Rgba32(0, 0, 0, 0));
       this.texts = new Stack<CanvasText>[this.Width, this.Height];
       foreach (CanvasText text in texts)
       {
@@ -81,13 +90,12 @@ namespace Core.Rendering
 
     public CanvasImageWithText AddText(CanvasText text)
     {
-      if(text.x < 0 || text.y < 0 || text.x >= this.Width || text.y >= this.Height)
-      {
+      if (text.x < 0 || text.y < 0 || text.x >= this.Width || text.y >= this.Height)
         return this;
-      }
-      if (this.texts[text.x, text.y] == null){
+
+      if (this.texts[text.x, text.y] == null)
         this.texts[text.x, text.y] = new Stack<CanvasText>();
-      }
+
       this.texts[text.x, text.y].Push(text);
       return this;
     }
@@ -96,19 +104,110 @@ namespace Core.Rendering
       this.texts = new Stack<CanvasText>[this.Width, this.Height];
     }
 
+    public void AddSubPixel(int x, int y, Rgb24 color)
+    {
+      if (x < 0 || y < 0 || x >= this.Width || y >= this.Height)
+        throw new ArgumentOutOfRangeException("Sub-pixel coordinates are out of bounds.");
+
+      SubPixels[x, y] = new Rgba32(color.R, color.G, color.B, 255);
+    }
+
+    public void ClearSubPixels()
+    {
+      SubPixels = new SixLabors.ImageSharp.Image<Rgba32>(this.Width, this.Height, new Rgba32(0, 0, 0, 0));
+    }
+    public void Clear(Rgb24 color)
+    {
+      Image.Mutate(i => i.Clear(color));
+      SubPixels.Mutate(i => i.Clear(new Rgba32(0, 0, 0, 0)));
+      ClearTexts();
+    }
+    
+    // https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+    public void AddLine((int x, int y) start, (int x, int y) end, Rgb24 color)
+    {
+      int x0 = start.x;
+      int y0 = start.y;
+      int x1 = end.x;
+      int y1 = end.y;
+
+      int dx = Math.Abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+      int dy = -Math.Abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
+      int err = dx + dy, e2; // error value e_xy
+      while (true)
+      {
+        if (x0 >= 0 && x0 < this.Image.Width && y0 >= 0 && y0 < this.Image.Height)
+        {
+          this.Image[x0, y0] = color; // Set the pixel
+        }
+
+        if (x0 == x1 && y0 == y1) break;
+        e2 = 2 * err;
+        if (e2 >= dy) { err += dy; x0 += sx; } // Move x direction
+        if (e2 <= dx) { err += dx; y0 += sy; } // Move y direction
+      }
+    }
+    public void AddLine(PointF[] points, Rgb24 color)
+    {
+      if (points.Length < 2)
+        throw new ArgumentException("At least two points are required to draw a line.");
+
+      for (int i = 0; i < points.Length - 1; i++)
+      {
+        AddLine(
+          ((int)points[i].X, (int)points[i].Y),
+          ((int)points[i + 1].X, (int)points[i + 1].Y),
+          color
+        );
+      }
+    }
+    public void AddSubPixelLine((int x, int y) start, (int x, int y) end, Rgb24 color)
+    {
+      int x0 = start.x;
+      int y0 = start.y;
+      int x1 = end.x;
+      int y1 = end.y;
+
+      int dx = Math.Abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+      int dy = -Math.Abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
+      int err = dx + dy, e2; // error value e_xy
+      while (true)
+      {
+        if (x0 >= 0 && x0 < this.Image.Width && y0 >= 0 && y0 < this.Image.Height)
+        {
+          this.SubPixels[x0, y0] = new Rgba32(color.R, color.G, color.B, 255); // Set the sub-pixel
+        }
+
+        if (x0 == x1 && y0 == y1) break;
+        e2 = 2 * err;
+        if (e2 >= dy) { err += dy; x0 += sx; } // Move x direction
+        if (e2 <= dx) { err += dx; y0 += sy; } // Move y direction
+      }
+    }
+    public void AddSubPixelLine(PointF[] points, Rgb24 color)
+    {
+      if (points.Length < 2)
+        throw new ArgumentException("At least two points are required to draw a line.");
+
+      for (int i = 0; i < points.Length - 1; i++)
+      {
+        AddSubPixelLine(
+          ((int)points[i].X, (int)points[i].Y),
+          ((int)points[i + 1].X, (int)points[i + 1].Y),
+          color
+        );
+      }
+    }
+
 
     protected override Measurement Measure(RenderOptions options, int maxWidth)
     {
       if (PixelWidth < 0)
-      {
         throw new InvalidOperationException("Pixel width must be greater than zero.");
-      }
 
       var width = MaxWidth ?? Width;
       if (maxWidth < width * PixelWidth)
-      {
         return new Measurement(maxWidth, maxWidth);
-      }
 
       return new Measurement(width * PixelWidth, width * PixelWidth);
     }
@@ -152,7 +251,7 @@ namespace Core.Rendering
         {
           var color = image[x, y];
 
-          if(currentText.Count > 0 && x > currentText.Peek().x + (currentText.Peek().text.Length - 1) / this.PixelWidth)
+          if (currentText.Count > 0 && x > currentText.Peek().x + (currentText.Peek().text.Length - 1) / this.PixelWidth)
           {
             currentText.Pop();
           }
@@ -173,11 +272,24 @@ namespace Core.Rendering
               {
                 break;
               }
-              pixel += currentText.Peek().text[(x - currentText.Peek().x ) * this.PixelWidth  + i];
+              pixel += currentText.Peek().text[(x - currentText.Peek().x) * this.PixelWidth + i];
             }
           }
           pixel = pixel.PadRight(PixelWidth, ' ');
-          yield return new Segment(pixel, new Style(background: new Color(color.R, color.G, color.B), foreground: currentText.Count > 0 ? new Color(currentText.Peek().color.R, currentText.Peek().color.G, currentText.Peek().color.B) : Color.Default));
+          if (SubPixels[x, y] != new Rgba32(0, 0, 0, 0))
+          {
+            for (int i = 0; i < PixelWidth; i++)
+            {
+              if (i % 2 == 0)
+                yield return new Segment(pixel[i].ToString(), new Style(background: new Spectre.Console.Color(SubPixels[x, y].R, SubPixels[x, y].G, SubPixels[x, y].B), foreground: currentText.Count > 0 ? new Spectre.Console.Color(currentText.Peek().color.R, currentText.Peek().color.G, currentText.Peek().color.B) : Spectre.Console.Color.Default));
+              else
+                yield return new Segment(pixel[i].ToString(), new Style(background: new Spectre.Console.Color(color.R, color.G, color.B), foreground: currentText.Count > 0 ? new Spectre.Console.Color(currentText.Peek().color.R, currentText.Peek().color.G, currentText.Peek().color.B) : Spectre.Console.Color.Default));
+            }
+          }
+          else
+          {
+            yield return new Segment(pixel, new Style(background: new Spectre.Console.Color(color.R, color.G, color.B), foreground: currentText.Count > 0 ? new Spectre.Console.Color(currentText.Peek().color.R, currentText.Peek().color.G, currentText.Peek().color.B) : Spectre.Console.Color.Default));
+          }
         }
 
         yield return Segment.LineBreak;
